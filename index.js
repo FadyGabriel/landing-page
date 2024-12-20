cb7ce8803cea56ba187d31840fe14ac3ae828a34
FadyGabriel <fadyghobrial123@gmail.com>
2024-12-20 17:37:41 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 
 let img = document.getElementById("main");
 let section = document.getElementById("section");
-
 function photos(x) {
     img.src = x;
 }
-
 function colors(color) {
     section.style.background = color;
 }
-
