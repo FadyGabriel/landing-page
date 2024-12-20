7e546edae2cb6ef3cd553a81219efb42798a3b68
FadyGabriel <fadyghobrial123@gmail.com>
2024-12-20 16:55:25 +0200

    first push

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let img = document.getElementById("main");
+let section = document.getElementById("section");
+
+function photos(x) {
+    img.src = x;
+}
+
+function colors(color) {
+    section.style.background = color;
+}
+
